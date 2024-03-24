7c2043ffee687013127cb67b909d29f41feeb672
varchasvipandey <varchasvip@gmail.com>
2024-03-24 11:44:12 +0530

    update: socket server handling

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,4 @@
 export const config = {
-  socketServerUrl: process.env.NEXT_PUBLIC_SOCKET_SERVER_URL!,
+  socketServerUrl:
+    process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_SOCKET_SERVER_URL_PROD! : process.env.NEXT_PUBLIC_SOCKET_SERVER_URL_DEV!,
 };
